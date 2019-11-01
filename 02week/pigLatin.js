c271121807a1d9ffdf2d253f7c18d54c10a6ff16
rodrieri000 <ericarodriguez7@outlook.com>
2019-10-31 19:12:17 -0500

    Updates

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -44,6 +44,18 @@ if (typeof describe === 'function') {
       assert.equal(pigLatin(' RoCkEt'), 'ocketray');
     });
   });
+
+  if (typeof describe === "function") {
+    describe("#pigLatin()", () => {
+      it("should translate a simple word", () => {
+        assert.equal(pigLatin("car"), "arcay");
+        assert.equal(pigLatin("dog"), "ogday");
+      });
+      it("should translate a complex word", () => {
+        assert.equal(pigLatin("create"), "eatecray");
+        assert.equal(pigLatin("valley"), "alleyvay");
+      });
+
 } else {
 
   getPrompt();
